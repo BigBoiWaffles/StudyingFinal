3dfdef6aa07d00f478e580a8dd4561b6b66dfdb3
Nicasio Balinag <nicasio.balinag@mail.utoronto.ca>
2026-01-09 09:11:42 -0500

    Starting new Iterator Example

---
+++ DesignPatterns/Iterator/BookCollection/BookCollection.java
@@ -0,0 +1,11 @@
+class BookCollection{
+  private ArrayList<Book> books;
+
+  public BookCollection(){
+    books = new ArrayList<>();
+  }
+
+  public void addBook(Book book){
+    books.add(book);
+  }
+}
